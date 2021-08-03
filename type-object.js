b3ed4fe5c46b4d598d4b32a93d209c648c0137d9
shihao0880 <shihao3108@gmail.com>
2021-08-03 20:33:29 +0700

    retore

--- type-object.js
+++ type-object.js
@@ -16,28 +16,38 @@
 
 // phương thức 1.1 dùng dấu , ngăn cách trong việc khai báo liển mạch
 // tất cả các khóa trong ký hiệu dấu ngoặc vuông được chuyển đổi thành chuỗi bằng phương thức ( object.toString ) trừ khi chúng là Ký hiệu (symbol)
-var myObj = new Object(),
-    str = 'myString',
-    rand = Math.random(),
-    obj = new Object();
+// var myObj = new Object(),
+//     str = 'myString',
+//     rand = Math.random(),
+//     obj = new Object();
 
-myObj.type              = 'Dot syntax';
-myObj['date created']   = 'String with space';
-myObj[str]              = 'String value';
-myObj[rand]             = 'Random Number';
-myObj[obj]              = 'Object';
-myObj['']               = 'Even an empty string';
-
-console.log(myObj);
+// myObj.type              = 'Dot syntax';
+// myObj['date created']   = 'String with space';
+// myObj[str]              = 'String value';
+// myObj[rand]             = 'Random Number';
+// myObj[obj]              = 'Object';
+// myObj['']               = 'Even an empty string';
+// console.log(myObj);
 
 // phương thức 2 : khai báo như 1 biến bình thường và dùng dấu ngoặc nhọn
-// var myCar = {
-//     make : 'Ford',
-//     model : 'Mustang',
-//     year : 1969 
-// };
-
+var myCar = {
+    make : 'Ford',
+    model : 'Mustang',
+    year : 1969 
+};
 // Trong trường hợp mà chúng ta truy cập vào một thuộc tính chưa khai báo thì nó sè trả về giá trị undefined ( và không là null )
 // console.log(myCar.color) ; // trả về giá trị undefined 
 
-
+//Chúng ta có 3 phương pháp cụ thể để liệt kê các thuộc tính trong 1 đối tượng
+ // dùng lệnh for ... in lặp, Phương thức này duyệt qua tất cả các thuộc tính có thể liệt kê của một đối tượng và chuỗi nguyên mẫu của nó.
+ function showProps(obj, objName) {
+    var result = ``;
+    for (var i in obj) {
+      // obj.hasOwnProperty() is used to filter out properties from the object's prototype chain
+      if (obj.hasOwnProperty(i)) {
+        result += `${objName}.${i} = ${obj[i]}\n`;
+      }
+    }
+    return result;
+  }
+  showProps(myCar, "myCar");
