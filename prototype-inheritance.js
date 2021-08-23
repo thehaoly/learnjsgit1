ff9ca549a368594173a51f4342990f24858bdf8f
shihao0880 <shihao3108@gmail.com>
2021-08-23 16:34:38 +0700

    create file prototype-inheritance learn knowlegde prototype and inheritance in js

---
+++ prototype-inheritance.js
@@ -0,0 +1,2 @@
+//@@@ Chúng ta sẽ tìm hiểu về chuổi nguyên mẫu và kế thừa trong javascript
+// Gần như tất cả các đối tượng trong JavaScript đều là các thể hiện của Đối tượng nằm trên cùng của chuỗi nguyên mẫu.
