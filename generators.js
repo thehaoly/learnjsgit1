e27dc683b4f4bbddf5d5a4f98860a393696ae49f
shihao0880 <shihao3108@gmail.com>
2021-08-27 23:06:46 +0700

    update knowlegde

--- generators.js
+++ generators.js
@@ -103,27 +103,107 @@
 
 //*** Sử dụng trình tạo để triển khai cấu trúc dữ liệu Túi
 // Túi là một cấu trúc dữ liệu có khả năng thu thập các phần tử và lặp lại qua các phần tử. Nó không hỗ trợ xóa các mục.
-class Bag {
-    constructor() {
-        this.elements = [];
-    }
-    isEmpty() {
-        return this.elements.length === 0;
-    }
-    add(element) {
-        this.elements.push(element);
-    }
-    * [Symbol.iterator]() {
-        for (let element of this.elements) {
-            yield element;
-        }
-    }
-}
-let bag = new Bag();
-bag.add(1);
-bag.add(2);
-bag.add(3);
-for (let e of bag) {
-    console.log(e);
-}
+// class Bag {
+//     constructor() {
+//         this.elements = [];
+//     }
+//     isEmpty() {
+//         return this.elements.length === 0;
+//     }
+//     add(element) {
+//         this.elements.push(element);
+//     }
+//     * [Symbol.iterator]() {
+//         for (let element of this.elements) {
+//             yield element;
+//         }
+//     }
+// }
+// let bag = new Bag();
+// bag.add(1);
+// bag.add(2);
+// bag.add(3);
+// for (let e of bag) {
+//     console.log(e);
+// }
 
+//@@@ Chúng ta nói về từ khóa yield và học cách làm sao sử dụng chúng trong trình khởi tạo ( Generator Function )
+// *** Giới thiệu về Yield keyword
+// Từ khóa yield cho phép bạn tạm dừng và tiếp tục một hàm của trình tạo ( generator function ) hay hàm* ( function* ).
+// Cú pháp cho quá trình sử dụng 
+// [variable_name] = yield [expression];
+// giải thích cú pháp :
+// Biểu thức chỉ định giá trị trả về từ một hàm trình tạo thông qua giao thức lặp. Nếu bạn bỏ qua biểu thức, yield sẽ trả về không xác định.
+// Tên_biến lưu trữ giá trị tùy chọn được truyền cho phương thức next () của đối tượng trình vòng lặp.
+//*** ví dụ : 
+//-** Trả về giá trị khi ứng dụng vào hàm generator function
+// function* foo() { 
+//     yield 1;
+//     yield 2;
+//     yield 3;
+// }
+// let f = foo();
+// console.log(f.next());
+// console.log(f.next());
+// console.log(f.next());
+// console.log(f.next());
+// console.log(f.next());
+// chúng ta thấy hàm đã được sử dụng 3 từ khóa yield và khi chúng ta gọi 3 lần, thì kết quả trả về cũng 3 lần với cập khóa gồm giá trị và hiện trạng
+// trong cặp khóa trả về có 1 đối số là value tất là giá trị, và một đối số done mang giá trị boolean là false, khi giải trị done = false thì có nghĩa là vòng lặp vẫn còn và ngược lại khi done = true thì không còn gí trị mặc dù ta có thể tiếp tục gọi hàm
+
+//-** Trả về giá trị undefinded
+// function* bar() {
+//     yield;
+// }
+// let b = bar();
+// console.log(b.next()); 
+
+//-** Truyền một giá trị cho phương thức next ()
+// Trong ví dụ sau, từ khóa lợi nhuận là một biểu thức đánh giá đối số được truyền đến phương thức next ():
+// function* generate() {
+//     let result = yield;
+//     console.log(`result is ${result}`);
+// }
+// let g = generate();
+// console.log(g.next()); 
+// console.log(g.next(1000)); 
+// Ta thấy câu lệnh đầu tiên trả về đối tượng { value: undefined, done: false }
+// Câu lệnh thứ hai nhận đối số là 1000 và thực hiện câu lệnh, sau khi thực hiện câu lệnh thì trả về đối tượng { value: undefined, done: true }
+// khi đối tượng trả về có cặp khóa giá trị done = true ; thì có nghĩa là vòng lập đã hết và không còn giá trị trả về
+
+//-** Sử dụng từ khóa yield trong một mảng
+// Ví dụ sau sử dụng từ khóa yield làm các phần tử của một mảng:
+// function* baz() {
+//     let arr = [yield, yield];
+//     console.log(arr);
+// }
+// var z = baz();
+// console.log(z.next());  
+// console.log(z.next(1)); 
+// console.log(z.next(2));
+// Lệnh gọi đầu tiên z.next () đặt phần tử đầu tiên của mảng arr thành 1 và trả về đối tượng { value: undefined, done: false }
+// Lệnh gọi thứ hai z.next () đặt thứ hai của mảng arr thành 2 và trả về đối tượng { value: undefined, done: false }
+// Lệnh gọi thứ ba z.next () hiển thị nội dung của mảng arr và trả về đối tượng [ 1, 2 ] { value: undefined, done: true }
+
+//-** Sử dụng yield để trả về một mảng
+// function* yieldArray() {
+//     yield 1;
+//     yield [ 20, 30, 40 ];
+// }
+// let y = yieldArray();
+// console.log(y.next()); 
+// console.log(y.next()); 
+// console.log(y.next());
+
+// //-** Sử dụng lợi nhuận để trả về các phần tử riêng lẻ của một mảng
+// function* yieldArrayElements() {
+//     yield 1;
+//     yield* [ 20, 30, 40 ];
+// }
+// let a = yieldArrayElements();
+// console.log(a.next()); // { value: 1, done: false }
+// console.log(a.next()); // { value: 20, done: false }
+// console.log(a.next()); // { value: 30, done: false }
+// console.log(a.next()); // { value: 40, done: false }
+// Trong ví dụ này,  yield* là cú pháp mới. Biểu thức yield* được sử dụng để ủy quyền cho một đối tượng hoặc trình tạo có thể lặp lại khác.
+// Kết quả là, biểu thức sau trả về các phần tử riêng lẻ của mảng [20, 30, 40]
---
+++ modules.js
@@ -0,0 +1,3 @@
+//@@@ Chúng ta sẽ tìm hiểu về cái nhìn toàn diện về mô đun trong ES6
+// trong hướng dẫn này, bạn sẽ tìm hiểu về các mô-đun ES6 và cách xuất các biến, hàm, lớp từ một mô-đun và sử dụng lại chúng trong các mô-đun khác
+// 
