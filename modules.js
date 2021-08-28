418cdf9eabd2b178c582834289a5b68b46c74f34
shihao0880 <shihao3108@gmail.com>
2021-08-28 16:53:45 +0700

    delete modules file

--- modules.js
+++
@@ -1,3 +0,0 @@
-//@@@ Chúng ta sẽ tìm hiểu về cái nhìn toàn diện về mô đun trong ES6
-// trong hướng dẫn này, bạn sẽ tìm hiểu về các mô-đun ES6 và cách xuất các biến, hàm, lớp từ một mô-đun và sử dụng lại chúng trong các mô-đun khác
-// 
